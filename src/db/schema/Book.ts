4a8cf07c7492904f5f1a7cddc0f21497bc38a80f
kennion <kennion@byu.edu>
2025-02-11 23:05:41 -0700

    Mark nullable fields as optional

--- src/db/schema/Book.ts
+++ src/db/schema/Book.ts
@@ -1,17 +1,17 @@
 interface Book {
   id: number;
   name: string;
-  isbn_list: string | null;
+  isbn_list?: string | null;
   author: string;
   primary_genre_id: number;
   audience_id: number;
-  pages: number | null;
-  series_id: number | null;
-  series_number: number | null;
-  publish_date: number | null; // YYYY
-  short_description: string | null;
-  language: string | null;
-  img_callback: string | null;
+  pages?: number | null;
+  series_id?: number | null;
+  series_number?: number | null;
+  publish_date?: number | null; // YYYY
+  short_description?: string | null;
+  language?: string | null;
+  img_callback?: string | null;
 }
 
 export type { Book };
