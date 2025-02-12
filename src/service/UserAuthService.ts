1707f2e44632cab3c6b6e73ec8da4518c632e090
kennion <kennion@byu.edu>
2025-02-11 22:15:49 -0700

    Add service class stubs

--- src/service/UserAuthService.ts
+++ src/service/UserAuthService.ts
@@ -1,33 +1,27 @@
-import jwt from 'jsonwebtoken'
+import jwt from "jsonwebtoken";
 
 export class UserAuthService {
+  constructor() {}
 
-    constructor() {
+  async login(username: string, password: string): Promise<string> {
+    const role = await this.getUserRole(username, password);
+    return await this.buildJWT(role);
+  }
 
-    }
+  async buildJWT(userRole: UserRole): Promise<string> {
+    const token = jwt.sign({ userRole }, process.env.AUTH_KEY, { expiresIn: "1h" });
+    console.log(`Token Generated: ${token}`);
+    return token;
+  }
 
-    async login (username: string, password: string): Promise<string> {
-        const role = await this.getUserRole(username, password)
-        return await this.buildJWT(role)
-    }
+  async getUserRole(username: string, password: string): Promise<UserRole> {
+    //TODO: make a call to sql db to get the user role associated with the login
 
-    async buildJWT (userRole: UserRole) {
-        const token = jwt.sign({ userRole }, process.env.AUTH_KEY, { expiresIn: '1h' })
-        console.log(`Token Generated: ${token}`)
-        return token
-    }
-
-    async getUserRole (username: string, password: string): Promise<UserRole> {
-        //TODO: make a call to sql db to get the user role associated with the login
-
-        return { campus: 'Lehi', roleType: 'Admin' }
-    }
-
-
-
+    return { campus: "Lehi", roleType: "Admin" };
+  }
 }
 
 export interface UserRole {
-    campus: string,
-    roleType: string
+  campus: string;
+  roleType: string;
 }
