ce320beaf86b4c01c37c8799713f6a7c2f94d888
Kelly Ko <kellyko2003@gmail.com>
2025-02-18 07:08:50 -0700

    error handling and get user email

--- src/routes/suggestRouter.ts
+++ src/routes/suggestRouter.ts
@@ -2,6 +2,7 @@ import express from "express";
 import { Config } from "../config";
 import nodemailer from "nodemailer";
 import cron from "node-cron";
+import Response from "../db/response/Response";
 
 const suggestRouter = express.Router();
 
@@ -17,20 +18,27 @@ const transporter = nodemailer.createTransport({
   },
 });
 
-cron.schedule("0 8 * * 5", () => {
-  Config.dependencies.suggestionService.emailSuggestionList(transporter);
+cron.schedule("0 8 * * 5", async () => {
+  const response: Response<any> =
+    await Config.dependencies.suggestionService.emailSuggestionList(
+      transporter
+    );
+  if (response.statusCode !== 200) {
+    console.log(response.message);
+  }
 });
 
-suggestRouter.post("/", (req, res) => {
+suggestRouter.post("/", async (req, res) => {
   const campus_name = req.body.campus;
   const suggestion_string = req.body.suggestion;
 
-  Config.dependencies.suggestionService.addSuggestion(
-    campus_name,
-    suggestion_string
-  );
+  const response: Response<any> =
+    await Config.dependencies.suggestionService.addSuggestion(
+      campus_name,
+      suggestion_string
+    );
 
-  res.sendStatus(200);
+  res.status(response.statusCode).send(response.message);
 });
 
 module.exports = { suggestRouter };
