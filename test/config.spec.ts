e98f7359cf2cdfa75c29aacded07065ab1211c3f
kennion <kennion@byu.edu>
2025-03-07 11:04:36 -0700

    Yeet config and index tests

--- test/config.spec.ts
+++
@@ -1,5 +0,0 @@
-describe("Config testing suite", () => {
-  test("Config tests not implemented yet", () => {
-    expect(1).toEqual(1);
-  });
-});
--- test/index.spec.ts
+++
@@ -1,5 +0,0 @@
-describe("Index testing suite", () => {
-  test("Index tests not implemented yet", () => {
-    expect(1).toEqual(1);
-  });
-});
