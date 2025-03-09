a4dc11fec6b4413ef74d5d47de4deb350322a959
kennion <kennion@byu.edu>
2025-03-08 20:53:51 -0700

    Add DAO factory to test setup

--- test/setup.ts
+++ test/setup.ts
@@ -1,10 +1,17 @@
+import DaoFactory from "../src/db/dao/DaoFactory";
 import TestConnectionManager from "./TestConnectionManager";
 
+export let testDaoFactory: DaoFactory;
+
 export default async function setup() {
-  // TODO: this should act like the config file to get handlers, etc. set up
+  // TODO: add Dynamo initialization stuff here because I have no idea how that is working currently
   console.log("Setting up test environment");
   await TestConnectionManager.initialize();
   console.log("Test database connection established");
   await TestConnectionManager.runCreateTestSQL();
   console.log("Creation of test database schema complete");
+
+  testDaoFactory = new DaoFactory(TestConnectionManager.kyselyDB);
+
+  console.log("Dependencies Instantiated");
 }
