7b6f4df3ac779cb8e160e631314d19734aa02532
Nikolas Earl <72423846+nearl2001@users.noreply.github.com>
2025-03-26 10:58:43 -0600

    fix testing and db stuff

--- src/config.ts
+++ src/config.ts
@@ -84,7 +84,7 @@ export class Config {
     dbConnectionManager.testConnection();
 
     if (process.env.NODE_ENV === "local") {
-      await dbConnectionManager.executeQuery("USE bibliotrace_v3");
+      await dbConnectionManager.executeQuery("USE bibliotrace_v3_test");
       await dbConnectionManager.runCreateSQL();
       await dbConnectionManager.runAddDummyData();
     }
