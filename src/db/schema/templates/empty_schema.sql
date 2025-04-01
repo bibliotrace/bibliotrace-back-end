6b104ecb6f933df76e60cbf95487cfeca7007627
kennion <kennion@byu.edu>
2025-03-31 20:13:57 -0600

    I forgot the tests depend on the table creation sequence

--- src/db/schema/templates/empty_schema.sql
+++ src/db/schema/templates/empty_schema.sql
@@ -2,29 +2,29 @@
 
 -- DROP TABLE IF EXISTS auth, audit_states, genres, genre_types, tags, shopping_list, restock_list, location, audiences, audit, audit_entry, campus, checkout, genre, tag, inventory, series, suggestions, users, user_roles, books, book_tag, book_genre ;
 
-CREATE TABLE audiences (
+CREATE TABLE IF NOT EXISTS audiences (
   id TINYINT UNSIGNED PRIMARY KEY AUTO_INCREMENT,
   audience_name VARCHAR(255) NOT NULL
 );
 
-CREATE TABLE series (
+CREATE TABLE IF NOT EXISTS series (
   id TINYINT UNSIGNED PRIMARY KEY AUTO_INCREMENT,
   series_name VARCHAR(255) NOT NULL,
   max_count TINYINT UNSIGNED
 );
 CREATE INDEX idx_series_name ON series(series_name);
 
-CREATE TABLE user_roles (
+CREATE TABLE IF NOT EXISTS user_roles (
   id TINYINT UNSIGNED PRIMARY KEY AUTO_INCREMENT,
   role_name VARCHAR(255) NOT NULL
 );
 
-CREATE TABLE campus (
+CREATE TABLE IF NOT EXISTS campus (
   id TINYINT UNSIGNED PRIMARY KEY AUTO_INCREMENT,
   campus_name VARCHAR(255) NOT NULL
 );
 
-CREATE TABLE location (
+CREATE TABLE IF NOT EXISTS location (
   id INT UNSIGNED PRIMARY KEY AUTO_INCREMENT,
   campus_id TINYINT UNSIGNED NOT NULL,
   location_name VARCHAR(255) NOT NULL,
@@ -32,17 +32,17 @@ CREATE TABLE location (
   FOREIGN KEY (campus_id) REFERENCES campus(id)
 );
 
-CREATE TABLE genre (
+CREATE TABLE IF NOT EXISTS genre (
   id INT UNSIGNED PRIMARY KEY AUTO_INCREMENT,
   genre_name VARCHAR(255) NOT NULL UNIQUE
 );
 
-CREATE TABLE tag (
+CREATE TABLE IF NOT EXISTS tag (
   id INT UNSIGNED PRIMARY KEY AUTO_INCREMENT,
   tag_name VARCHAR(255) NOT NULL UNIQUE
 );
 
-CREATE TABLE books (
+CREATE TABLE IF NOT EXISTS books (
   id INT UNSIGNED PRIMARY KEY AUTO_INCREMENT,
   book_title VARCHAR(255) NOT NULL,
   isbn_list VARCHAR(255), -- this unfortunately needs to be nullable because some books come in without an isbn
@@ -63,7 +63,7 @@ CREATE TABLE books (
 CREATE UNIQUE INDEX idx_name ON books(book_title);
 
 
-CREATE TABLE book_genre (
+CREATE TABLE IF NOT EXISTS book_genre (
   id INT UNSIGNED PRIMARY KEY AUTO_INCREMENT,
   book_id INT UNSIGNED NOT NULL,
   genre_id INT UNSIGNED NOT NULL,
@@ -71,7 +71,7 @@ CREATE TABLE book_genre (
   FOREIGN KEY (genre_id) REFERENCES genre(id) ON DELETE CASCADE
 );
 
-CREATE TABLE book_tag (
+CREATE TABLE IF NOT EXISTS book_tag (
   id INT UNSIGNED PRIMARY KEY AUTO_INCREMENT,
   book_id INT UNSIGNED NOT NULL,
   tag_id INT UNSIGNED NOT NULL,
@@ -79,7 +79,7 @@ CREATE TABLE book_tag (
   FOREIGN KEY (tag_id) REFERENCES tag(id)
 );
 
-CREATE TABLE inventory (
+CREATE TABLE IF NOT EXISTS inventory (
   qr VARCHAR(15) PRIMARY KEY,
   book_id INT UNSIGNED NOT NULL,
   location_id INT UNSIGNED NOT NULL,
@@ -93,7 +93,7 @@ CREATE INDEX idx_location ON inventory(location_id);
 CREATE INDEX idx_campus_id ON campus(id);
 CREATE INDEX idx_ttl ON inventory(ttl);
 
-CREATE TABLE audit (
+CREATE TABLE IF NOT EXISTS audit (
   id TINYINT UNSIGNED PRIMARY KEY AUTO_INCREMENT,
   campus_id TINYINT UNSIGNED NOT NULL,
   start_date DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
@@ -101,7 +101,7 @@ CREATE TABLE audit (
   FOREIGN KEY (campus_id) REFERENCES campus(id)
 );
 
-CREATE TABLE audit_entry (
+CREATE TABLE IF NOT EXISTS audit_entry (
   qr VARCHAR(15) NOT NULL,
   audit_id TINYINT UNSIGNED NOT NULL,
   state ENUM('Missing', 'Misplaced', 'Found', 'Extra'),
@@ -110,7 +110,7 @@ CREATE TABLE audit_entry (
   FOREIGN KEY (audit_id) REFERENCES audit(id)
 );
 
-CREATE TABLE checkout (
+CREATE TABLE IF NOT EXISTS checkout (
   checkout_id INT UNSIGNED PRIMARY KEY AUTO_INCREMENT,
   timestamp TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
   qr VARCHAR(15) NOT NULL,
@@ -121,7 +121,7 @@ CREATE TABLE checkout (
 CREATE INDEX idx_qr ON checkout(qr);
 CREATE INDEX idx_book_id ON checkout(book_id);
 
-CREATE TABLE suggestions (
+CREATE TABLE IF NOT EXISTS suggestions (
   suggestion_id INT UNSIGNED PRIMARY KEY AUTO_INCREMENT,
   timestamp TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
   content TEXT NOT NULL,
@@ -129,7 +129,7 @@ CREATE TABLE suggestions (
   FOREIGN KEY (campus_id) REFERENCES campus(id)
 );
 
-CREATE TABLE users (
+CREATE TABLE IF NOT EXISTS users (
   username VARCHAR(255) PRIMARY KEY,
   password_hash VARCHAR(255) NOT NULL,
   role_id TINYINT UNSIGNED NOT NULL,
@@ -139,7 +139,7 @@ CREATE TABLE users (
   FOREIGN KEY (campus_id) REFERENCES campus(id)
 );
 
-CREATE TABLE shopping_list (
+CREATE TABLE IF NOT EXISTS shopping_list (
   book_id INT UNSIGNED PRIMARY KEY,
   title VARCHAR(255) NOT NULL,
   author VARCHAR(255) NOT NULL,
@@ -148,7 +148,7 @@ CREATE TABLE shopping_list (
   FOREIGN KEY (book_id) REFERENCES books(id)
 );
 
-CREATE TABLE restock_list (
+CREATE TABLE IF NOT EXISTS restock_list (
   book_id INT UNSIGNED PRIMARY KEY,
   title VARCHAR(255) NOT NULL,
   author VARCHAR(255) NOT NULL,
