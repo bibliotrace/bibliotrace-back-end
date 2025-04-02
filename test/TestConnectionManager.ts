e40bb31c63843078da437421bd98450bd2df509c
Nikolas Earl <72423846+nearl2001@users.noreply.github.com>
2025-04-02 09:52:59 -0600

    chore(dao test): just use the root user for testing

--- test/TestConnectionManager.ts
+++ test/TestConnectionManager.ts
@@ -21,9 +21,12 @@ class TestConnectionManager {
       throw error;
     }
 
-    const user = process.env.GITHUB_ACTIONS ? "root" : "admin"
-    const password = process.env.GITHUB_ACTIONS ? "password" : "Bibl!otrace_2025"
+    // const user = process.env.GITHUB_ACTIONS ? "root" : "admin"
+    // const password = process.env.GITHUB_ACTIONS ? "password" : "Bibl!otrace_2025"
     // console.log(user, password, 'Database Credentials Used')
+
+    const user = "root"
+    const password = "password"
 
     this.pool = createPool({
       host: "localhost",
@@ -42,8 +45,8 @@ class TestConnectionManager {
   private async createTestDatabase(): Promise<void> {
     const connection = createPool({
       host: "localhost",
-      user: process.env.GITHUB_ACTIONS ? "root" : "admin",
-      password: process.env.GITHUB_ACTIONS ? "password" : "Bibl!otrace_2025",
+      user: "root",
+      password: "password",
     });
 
     try {
